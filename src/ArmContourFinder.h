e1f2eb85d20884527894f9904416a1e61c5abf11
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-05-12 11:17:33 -0400

    Tried to refactor, gave up

--- src/ArmContourFinder.h
+++ src/ArmContourFinder.h
@@ -19,7 +19,31 @@ public:
 	map< unsigned int, int > side;
 
 	void update();
-	ofPolyline getHand(int n);
+
+	struct Hand
+	{
+		Hand():
+			centroid( ofPoint(-1, -1) ),
+			tip( ofPoint(-1, -1) ),
+			end( ofPoint(-1, -1) ),
+			boxCenter( ofPoint(-1, -1) ) {}
+		ofPolyline line;
+		ofPoint centroid;
+		ofPoint tip;
+		ofPoint end;
+		ofPoint boxCenter;
+		vector< ofPoint > wrists;
+		int side;
+		unsigned int label;
+		ofVec2f velocity;
+
+		//For sorting by label
+		bool operator < (const Hand& str) const
+		{
+			return (label < str.label);
+		}
+	};
+	map< unsigned int, Hand > hands;
 	
 	int MIN_HAND_SIZE;
 	int MAX_HAND_SIZE;
@@ -31,31 +55,12 @@ public:
 private:
 
 	bool findHand(int n);
-	ofPoint			 	findEnd(int n);
-	ofPoint				findTip(int n);
-	vector< ofPoint > 	findWrists(int n);
-	ofPoint 			refitTip(int n);
+	ofPoint			 	findEnd(int n, Hand newHand);
+	ofPoint				findTip(int n, Hand newHand);
+	vector< ofPoint > 	findWrists(int n, Hand newHand);
+	ofPoint 			refitTip(int n, Hand newHand);
 
 	void addHand(int n);
 
-	struct Hand
-	{
-		ofPolyline line;
-		ofPoint centroid;
-		ofPoint tip;
-		ofPoint end;
-		ofPoint boxCenter;
-		vector< ofPoint > wrists;
-		int index;
-		unsigned int label;
-		ofVec2f velocity;
-
-		//For sorting by label
-		bool operator < (const Hand& str) const
-		{
-			return (label < str.label);
-		}
-	};
-	vector< Hand > hands;
 	
 };
